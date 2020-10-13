c8ba5ee71991c47e9b09716909ea42dbcaaa2461
Dani-Asus <danielsosa1111@hotmail.com>
2020-10-13 02:03:24 -0300

    v1

--- app/src/main/java/com/example/tf/ui/cerrar_sesion/CerrarSesion.java
+++ app/src/main/java/com/example/tf/ui/cerrar_sesion/CerrarSesion.java
@@ -2,11 +2,14 @@ package com.example.tf.ui.cerrar_sesion;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,7 +28,9 @@ public class CerrarSesion extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.cerrar_sesion_fragment, container, false);
+        View view = inflater.inflate(R.layout.cerrar_sesion_fragment, container, false);
+        inicializar();
+        return view;
     }
 
     @Override
@@ -33,6 +38,26 @@ public class CerrarSesion extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(CerrarSesionViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    public void inicializar(){
+        new AlertDialog.Builder(getContext())
+                .setTitle("Salida")
+                .setMessage("Desea Salir S/N")
+                .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.perfil);
+                    }
+                }).show();
+
+
     }
 
 }
